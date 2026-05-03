04764f8efaea0b65159ddbe447a9b6e82db9751a
dywlphy <87897108+dywlphy@users.noreply.github.com>
2026-05-03 14:30:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,21 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.defa
 
 # 添加包含 CUPS 等打印包的第三方源
 echo 'src-git cupspackages https://github.com/Gr4ffy/lede-cups.git' >> feeds.conf.default
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# 添加包含 helloworld 的源（你原有的）
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+
+# 添加包含 CUPS 等打印包的第三方源
+echo 'src-git cupspackages https://github.com/Gr4ffy/lede-cups.git' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
