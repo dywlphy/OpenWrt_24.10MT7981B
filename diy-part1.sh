0af24638b1e725d4a176d3793e7990c1788c6901
dywlphy <87897108+dywlphy@users.noreply.github.com>
2026-05-13 01:12:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,6 +8,6 @@ echo "src-git luci https://github.com/openwrt/luci.git;openwrt-24.10" >> feeds.c
 echo "src-git smpackage https://github.com/dywlphy/small-package" >> feeds.conf
 echo "src-git immortalwrt https://github.com/immortalwrt/packages" >> feeds.conf
 # 打印源
-echo "src-git printing https://github.com/dywlphy/openwrt-feed-printing" >> feeds.conf
+# echo "src-git printing https://github.com/dywlphy/openwrt-feed-printing" >> feeds.conf
 echo "src-git hplipfeed https://github.com/woniuzfb/openwrt-24-printing-packages" >> feeds.conf
 echo "✅ feeds.conf 配置完成（7个源）"
